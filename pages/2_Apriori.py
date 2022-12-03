17748ad50ce817b29a37d67cb57505e780d3f3bf
bbydino <daniel.ngo16@gmail.com>
2022-12-02 21:32:32 -0600

    more to apriori web page

--- pages/2_Apriori.py
+++ pages/2_Apriori.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 
 import apriori
@@ -7,36 +6,14 @@ st.markdown(
     (
         "### Enter a movie and Cinema Suggestions will show you 10 "
         "of the most similar movies based movies that other users liked "
-        "when watching that movie, using the Apriori Algorithm"
+        "when also liking that movie, using the Apriori Algorithm"
     )
 )
 
 # only needs to do this one time, so adding the cache statement will make it so this function only runs the first time the page is loaded but doesn't run again as long as the user doesn't close the tab in their browser
 @st.cache
 def load_data():
-    # load the data
-    ratings_df = pd.read_csv("Data_Files/ratings_small.csv")
-    movies_df = pd.read_csv("Data_Files/movies_filtered.csv")
-
-    # remove movies with blank titles
-    title_mask = movies_df["title"].isna()
-    movies_df = movies_df.loc[title_mask == False]
-
-    # merge the 2 df on common column: movieId (must first convert col to int). similar to SQL Join
-    movies_df = movies_df.astype({"id": "int64"})
-    df = pd.merge(
-        ratings_df, movies_df[["id", "title"]], left_on="movieId", right_on="id"
-    )
-
-    # remove duplicated column (movieId), and useless column (timestamp)
-    df = df.drop(["timestamp", "id"], axis=1)
-
-    # userId is index, columns are movie tiles, and the values are the ratings
-    df = df.drop_duplicates(["userId", "title"])
-    df = df.pivot(index="userId", columns="title", values="rating").fillna(0)
-    df = df.astype("int64")
-
-    return (movies_df, ratings_df, df)
+    return apriori.data_preprocessing()
 
 
 movies_df, ratings_df, apriori_df = load_data()
@@ -75,7 +52,7 @@ metric_threshold = st.slider(
 start_apriori = st.button("Create association rules with apriori")
 
 # get movie from user via dropdown menu that has all the movies from the preprocessed set
-user_movie = st.selectbox(
+user_movie_original = st.selectbox(
     "Select a movie:",
     [title for title in movies_df["original_title"]],
 )
@@ -83,50 +60,71 @@ user_movie = st.selectbox(
 # max number of recommended movies
 max_movies = st.slider("Choose a maximum number of recommended movies:", 0, 10, 7, 1)
 
+# button to get recommended movies
+start_recommend = st.button("Find recommendations")
+
 # convert from original title to the title used in the algorithm
-movie_idx = movies_df.index[movies_df["original_title"] == user_movie][0]
+movie_idx = movies_df.index[movies_df["original_title"] == user_movie_original][0]
 user_movie = movies_df["title"][movie_idx]
 
 # re-generate association rules when button is clicked
 if start_apriori:
+    # encode the ratings in the df based on user input rating threshold
+    apriori_df = apriori_df.applymap(
+        apriori.encode_ratings, None, rating_threshold=rating_threshold
+    )
+
     # generate frequent itemsets using apriori
-    st.markdown("Apriori Running...")
-    freq_itemsets = apriori.apriori(
+    st.session_state.freq_itemsets = apriori.apriori(
         df=apriori_df, min_support=min_support, max_len=max_len
     )
 
     # generate rules from frequent itemsets
-    rules_df = apriori.create_association_rules(
-        frequent_itemsets=freq_itemsets,
+    st.session_state.rules_df = apriori.create_association_rules(
+        frequent_itemsets=st.session_state.freq_itemsets,
         metric=metric,
         metric_threshold=metric_threshold,
     ).sort_values(by=[metric], ascending=False)
 
-    st.markdown("Apriori Completed!")
+if "rules_df" in st.session_state and start_recommend:
+    # get recommended movies from rules
+    (
+        st.session_state.user_movie_rules_df,
+        st.session_state.recommended_movies,
+    ) = apriori.recommend_movies_apriori(
+        movie_title=user_movie,
+        rules_df=st.session_state.rules_df,
+        max_movies=max_movies,
+    )
 
+if "freq_itemsets" in st.session_state and "rules_df" in st.session_state:
+    st.markdown(
+        "##### Frequent Itemsets with `rating_threshold=%0.2f`, `min_support=%0.2f`"
+        % (rating_threshold, min_support),
+    )
+    st.dataframe(st.session_state.freq_itemsets)
 
-# get recommended movies from rules
-user_movie_rules_df, recommended_movies = apriori.recommend_movies_apriori(
-    movie_title=user_movie, rules_df=rules_df, max_movies=max_movies
-)
+    st.markdown(
+        "##### Association Rules with `metric=%s`, `metric_threshold=%0.2f`"
+        % (metric, metric_threshold)
+    )
+    st.dataframe(st.session_state.rules_df)
 
-st.markdown(
-    "##### Frequent Itemsets with `rating_threshold=%f`, `min_support=%f`"
-    % (rating_threshold, min_support),
-)
-st.dataframe(freq_itemsets)
+if (
+    "user_movie_rules_df" in st.session_state
+    and "recommended_movies" in st.session_state
+):
+    st.markdown(
+        "##### Association Rules for `movie=%s` with `metric=%s`, `metric_threshold=%0.2f`"
+        % (user_movie, metric, metric_threshold)
+    )
+    st.dataframe(st.session_state.user_movie_rules_df)
 
-st.markdown(
-    "##### Association Rules with `metric=%s`, `metric_threshold=%f`"
-    % (metric, metric_threshold)
-)
-st.dataframe(rules_df)
+    st.markdown(
+        "##### Found `%d` recommendations based off users who liked `%s`"
+        % (len(st.session_state.recommended_movies), user_movie_original)
+    )
 
-st.markdown(
-    "##### Association Rules for `movie=%s` with `metric=%s`, `metric_threshold=%f`"
-    % (user_movie, metric, metric_threshold)
-)
-st.dataframe(user_movie_rules_df)
-
-st.markdown("##### %d Most Similar Movies to `%s`" % (max_movies, user_movie))
-st.markdown(recommended_movies)
+    st.markdown(st.session_state.recommended_movies)
+    for movie in st.session_state.recommended_movies:
+        st.markdown("`%s`" % movie)
