631f68cf17ef6d80114e14506925eba43994f52b
bbydino <daniel.ngo16@gmail.com>
2022-12-04 22:21:53 -0600

    add timer to apriori UI

--- pages/2_Apriori.py
+++ pages/2_Apriori.py
@@ -1,5 +1,5 @@
-import numpy as np
-import pandas as pd
+import time
+
 import streamlit as st
 
 import apriori
@@ -44,7 +44,7 @@ c1, c2 = st.columns((1, 1))
 METRICS = ["support", "confidence", "lift"]
 c1.markdown("#### Rule Generation (Apriori)")
 rating_threshold = c1.slider(
-    "Minimum `good` movie rating for apriori", 0.0, 10.0, 6.0, 0.1
+    "Minimum `good` movie rating for apriori", 6.0, 10.0, 6.0, 0.1
 )
 min_support = c1.slider("Minimum support value for apriori", 0.02, 1.0, 0.07, 0.01)
 max_k_itemsets = c1.slider("Maximum itemset size for apriori", 1, 5, 5)
@@ -59,10 +59,11 @@ start_apriori = c1.button("GENERATE ASSOCIATION RULES")
 # max number of recommended movies
 c2.markdown("#### Recommend Movies")
 user_movie = c2.selectbox("Movie:", [title for title in movies_df["original_title"]])
-max_movies = c2.slider("Maximum number of movie recommendations", 1, 10, 1)
+max_movies = c2.slider("Maximum number of movie recommendations", 1, 10, 10, 1)
 start_recommend = c2.button("FIND RECOMMENDATIONS")
 
 if start_apriori:
+    st.session_state.time_apriori_start = time.time()
     # encode the ratings in the df to True/False
     # divide threshold by 2 because ratings in df are from 0-5, instead of 0-10
     apriori_df = apriori_df.applymap(
@@ -80,15 +81,28 @@ if start_apriori:
         metric=metric,
         metric_threshold=metric_threshold,
     )
+    st.session_state.time_apriori_end = time.time()
 
 if "rules_df" in st.session_state and "freq_itemsets" in st.session_state:
     # show user
     # make copy to prevent streamlit "autofixes" (which converts frozenset to string)
     c1.markdown("#### Association Rules")
-    c1.dataframe(format_rules_df(st.session_state.rules_df))
+    c1.markdown(
+        "Apriori finished execution in: `%.4f` seconds"
+        % (st.session_state.time_apriori_end - st.session_state.time_apriori_start)
+    )
+    rules_df = format_rules_df(st.session_state.rules_df)
+
+    # check that dataframe is within memory limits to display
+    if rules_df.memory_usage(deep=True).sum() < 200 * 1e6:
+        c1.dataframe(rules_df)
+    else:
+        c1.warning("The DataFrame was too large to display, so here's a part of it.")
+        c1.dataframe(rules_df.head(1000))
 
 if start_recommend:
     if "rules_df" in st.session_state and "freq_itemsets" in st.session_state:
+        time_start = time.time()
         # get movie recommendations
         user_movie_rules_df, recommended_movies_df = apriori.recommend_movies_apriori(
             movie_title=user_movie,
@@ -100,6 +114,10 @@ if start_recommend:
 
         # show user the recommended movies
         c2.markdown("#### Recommended Movies (`%s`)" % user_movie)
+        c2.markdown(
+            "Found `%d` movie recommendations in: `%.4f` seconds"
+            % (recommended_movies_df.shape[0], (time.time() - time_start))
+        )
         if not recommended_movies_df.empty:
             c2.table(
                 recommended_movies_df.style.format(
@@ -111,7 +129,9 @@ if start_recommend:
             )
 
             # make copy to prevent streamlit "autofixes" (which converts frozenset to string)
-            c2.markdown("#### Association Rules Containing `%s`" % user_movie)
+            c2.markdown(
+                "#### Association Rules containing `%s` as antecedent" % user_movie
+            )
             c2.dataframe(format_rules_df(user_movie_rules_df))
         else:
             c2.info(
@@ -120,6 +140,5 @@ if start_recommend:
                 "Please try another movie (one that is in the antecedent of a rule)."
                 % user_movie
             )
-
     else:
         c2.error("Please generate association rules first!")
